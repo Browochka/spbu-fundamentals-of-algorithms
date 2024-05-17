0d015a4aa1c695bf700077e0ecd78c52b0903a2f
Browka <okkarunko@gmail.com>
2024-05-17 18:14:57 +0300

    [SOvchinnikov] homework 2,3,5,6,7'

--- practicum_6/homework/basic/cholesky.py
+++ practicum_6/homework/basic/cholesky.py
@@ -2,22 +2,36 @@ import numpy as np
 
 
 def cholesky(A):
+    L = np.zeros_like(A)
+    for i in range(A.shape[0]):
+        for j in range(i+1):
+            if i == j:
+                summ=np.sum(L[i,:j]**2)
+                L[i, j] = np.sqrt(A[i, i] - summ)
+            else:
+                summ = np.sum(L[i, :j] * L[j, :j])
+                L[i, j] = (A[i, j] - summ) / L[j, j]
+    return L
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
 
-    pass
 
 
 if __name__ == "__main__":
-    L = np.array(
+    L_real = np.array(
         [
             [1.0, 0.0, 0.0],
             [4.0, 2.0, 0.0],
             [6.0, 5.0, 3.0],
         ]
     )
-    A = L @ L.T
-    L = cholesky(A)
-    print(L)
+    A = L_real @ L_real.T
+    print("A:")
+    print(A)
+
+    L_new = cholesky(A)
+    print("\nCholesky L:")
+    print(L_new)
+
+    A_new = L_new @ L_new.T
+    print("\n L @ L.T:")
+    print(A_new)
